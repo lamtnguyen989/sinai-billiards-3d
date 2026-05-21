48b0b09d8f1fd73473fbf679c2e08874f89e93a9
Lam Nguyen <lamtnguyen989@gmail.com>
2026-05-21 16:48:17 -0500

    tangent vector arithmetic

--- src/ergodic.rs
+++ src/ergodic.rs
@@ -25,6 +25,67 @@ impl TangentVector
     fn norm(self) -> f64 {return self.dot(self).sqrt();}
 }
 
+/// Overloading operators for arithmetics ///
+// Addition 
+impl std::ops::Add for TangentVector 
+{
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        return Self {
+            dp: self.dp + other.dp,
+            dq: self.dq + other.dq
+        }
+    }
+}
+
+impl std::ops::AddAssign for TangentVector
+{
+    fn add_assign(&mut self, other: Self) {
+        self.dp += other.dp;
+        self.dq += other.dq;
+    }
+}
+
+// Subtraction
+impl std::ops::Sub for TangentVector
+{
+    type Output = Self;
+    fn sub(self, other: Self) -> Self {
+        return Self {
+            dp: self.dp - other.dp,
+            dq: self.dq - other.dq
+        }
+    }
+}
+
+impl std::ops::SubAssign for TangentVector
+{
+    fn sub_assign(&mut self, other: Self) {
+        self.dp -= other.dp;
+        self.dq -= other.dq;
+    }
+}
+
+// Scalar multiplication
+impl std::ops::Mul<f64> for TangentVector
+{
+    type Output = Self;
+    fn mul(self, a: f64) -> Self {
+        return Self {
+            dp : a * self.dp,
+            dq : a * self.dq
+        }
+    }
+}
+
+impl std::ops::MulAssign<f64> for TangentVector
+{
+    fn mul_assign(&mut self, a: f64) {
+        self.dp *= a;
+        self.dq *= a;
+    }
+}
+
 /* Billiards trajectory */
 #[derive(Clone)]
 pub struct Trajectory
