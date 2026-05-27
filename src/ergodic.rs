ff4416f6b174fdf242a6f67c8698e2b53ba53417
Lam Nguyen <lamtnguyen989@gmail.com>
2026-05-27 01:26:26 -0500

    organizing

--- src/ergodic.rs
+++ src/ergodic.rs
@@ -1,108 +1,10 @@
-use crate::physics::*;
+
 
 use glam::Vec3;
 use glam::DVec3;
+use crate::physics::*;
 
 /* Constants */
-pub const NUM_TANGENTS: usize = 6;
-
-/* Tangent vector */
-#[derive(Clone, Copy, Default)]
-struct TangentVector
-{
-    dp: DVec3,  // Position tangent
-    dq: DVec3,  // Momentum tangent
-}
-
-impl TangentVector
-{
-    // Dot product
-    fn dot(self, other: Self) -> f64 {
-        return self.dq.dot(other.dq) + self.dp.dot(other.dp);
-    }
-
-    // Norm
-    fn norm(self) -> f64 {return self.dot(self).sqrt();}
-}
-
-/// Overloading operators for arithmetics ///
-// Addition 
-impl std::ops::Add for TangentVector 
-{
-    type Output = Self;
-    fn add(self, other: Self) -> Self {
-        return Self {
-            dp: self.dp + other.dp,
-            dq: self.dq + other.dq
-        }
-    }
-}
-
-impl std::ops::AddAssign for TangentVector
-{
-    fn add_assign(&mut self, other: Self) {
-        self.dp += other.dp;
-        self.dq += other.dq;
-    }
-}
-
-// Subtraction
-impl std::ops::Sub for TangentVector
-{
-    type Output = Self;
-    fn sub(self, other: Self) -> Self {
-        return Self {
-            dp: self.dp - other.dp,
-            dq: self.dq - other.dq
-        }
-    }
-}
-
-impl std::ops::SubAssign for TangentVector
-{
-    fn sub_assign(&mut self, other: Self) {
-        self.dp -= other.dp;
-        self.dq -= other.dq;
-    }
-}
-
-// Scalar multiplication
-impl std::ops::Mul<f64> for TangentVector
-{
-    type Output = Self;
-    fn mul(self, a: f64) -> Self {
-        return Self {
-            dp : a * self.dp,
-            dq : a * self.dq
-        }
-    }
-}
-
-impl std::ops::MulAssign<f64> for TangentVector
-{
-    fn mul_assign(&mut self, a: f64) {
-        self.dp *= a;
-        self.dq *= a;
-    }
-}
-
-/* Billiards trajectory */
-#[derive(Clone)]
-pub struct Trajectory
-{
-    pub positions:                  Vec<glam::Vec3>,
-    pub velocities:                 Vec<glam::Vec3>,
-    pub current_lyapunov_spectra:   [f64; NUM_TANGENTS],
-    pub kaplan_yorke_dim:           f64,
-}
-
-impl Trajectory
-{
-
-    // Getters for current position and velocities
-    pub fn current_pos(&self) -> Vec3 {return *self.positions.last().unwrap();}
-    pub fn current_vel(&self) -> Vec3 {return *self.velocities.last().unwrap();}
-}
 
 /* Ergodic Statistics */
 #[derive(Default, Clone)]
@@ -118,6 +20,7 @@ pub struct ErgodicStats
 
 // Kaplan-Yorke dimension
 // IMPORTANT: This assume we have sorted the spectra (not sorting here since there can be a way to shoehorn the sorting somewhere else)
+// Also this is most likely zero for this billiards model although it is worth it to give it a proper treatment
 fn kaplan_yorke_dim(lya_spectra: &[f64]) -> f64
 {
     let mut sum: f64 = 0.0;
--- src/physics.rs
+++ src/physics.rs
@@ -1,18 +1,19 @@
 use rand::Rng;
 use glam::Vec3;
+use glam::DVec3;
 
 /*** 
-*   Geometry macros 
+*   Geometry constants
 ***/
 pub const BOX_SIZE      : f32 = 1.0;
 pub const SPHERE_RADIUS : f32 = 0.25;
 pub const SPHERE_CENTER : Vec3 = Vec3::new(0.5, 0.5, 0.5);
+pub const NUM_TANGENTS  : usize = 6;
 const PHYS_EPSILON      : f32 = 1e-5;   // Physics error margin
 
 /***
 *   Reflections 
 ***/
-
 // Sphere reflections
 fn reflection_sphere(pos: Vec3, vel: Vec3) -> Vec3
 {
@@ -120,4 +121,107 @@ pub fn collision(pos: Vec3, vel: Vec3) -> Option<(Vec3, Vec3)>
                         else {reflection_box(new_pos, v)};
 
     return Some((new_pos, new_vel));
+}
+
+
+/***
+    Tangent vector 
+***/
+#[derive(Clone, Copy, Default)]
+struct TangentVector
+{
+    dp: DVec3,  // Position tangent
+    dq: DVec3,  // Momentum tangent
+}
+
+impl TangentVector
+{
+    // Dot product
+    fn dot(self, other: Self) -> f64 {
+        return self.dq.dot(other.dq) + self.dp.dot(other.dp);
+    }
+
+    // Norm
+    fn norm(self) -> f64 {return self.dot(self).sqrt();}
+}
+
+/// Overloading operators for arithmetics ///
+// Addition 
+impl std::ops::Add for TangentVector 
+{
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        return Self {
+            dp: self.dp + other.dp,
+            dq: self.dq + other.dq
+        }
+    }
+}
+
+impl std::ops::AddAssign for TangentVector
+{
+    fn add_assign(&mut self, other: Self) {
+        self.dp += other.dp;
+        self.dq += other.dq;
+    }
+}
+
+// Subtraction
+impl std::ops::Sub for TangentVector
+{
+    type Output = Self;
+    fn sub(self, other: Self) -> Self {
+        return Self {
+            dp: self.dp - other.dp,
+            dq: self.dq - other.dq
+        }
+    }
+}
+
+impl std::ops::SubAssign for TangentVector
+{
+    fn sub_assign(&mut self, other: Self) {
+        self.dp -= other.dp;
+        self.dq -= other.dq;
+    }
+}
+
+// Scalar multiplication
+impl std::ops::Mul<f64> for TangentVector
+{
+    type Output = Self;
+    fn mul(self, a: f64) -> Self {
+        return Self {
+            dp : a * self.dp,
+            dq : a * self.dq
+        }
+    }
+}
+
+impl std::ops::MulAssign<f64> for TangentVector
+{
+    fn mul_assign(&mut self, a: f64) {
+        self.dp *= a;
+        self.dq *= a;
+    }
+}
+
+/*** 
+    Billiards trajectory 
+***/
+#[derive(Clone)]
+pub struct Trajectory
+{
+    pub positions:                  Vec<glam::Vec3>,
+    pub velocities:                 Vec<glam::Vec3>,
+    pub current_lyapunov_spectra:   [f64; NUM_TANGENTS],
+    pub kaplan_yorke_dim:           f64,
+}
+
+impl Trajectory
+{
+
+    // Getters for current position and velocities
+    pub fn current_pos(&self) -> Vec3 {return *self.positions.last().unwrap();}
+    pub fn current_vel(&self) -> Vec3 {return *self.velocities.last().unwrap();}
 }
