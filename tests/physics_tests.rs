cecbb2274c0faec0a4d6646692961919f82013c7
Lam Nguyen <lamtnguyen989@gmail.com>
2026-05-29 05:04:22 -0500

    refining tangent phase vector

--- tests/physics_tests.rs
+++ tests/physics_tests.rs
@@ -15,28 +15,35 @@ fn collision_remains_in_box() {
 }
 
 #[test]
-fn test_phase_point_creation() {
+fn test_phase_tangent_creation() {
     assert_eq!(NUM_TANGENTS, 6);
     let arr_val: [f64; NUM_TANGENTS] = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
-    let pt = PhasePoint::from_array(arr_val);
+    let pt = TangentPhaseVector::from_array(arr_val);
 
-    assert_eq!(pt.get_position(), DVec3::new(arr_val[0], arr_val[1], arr_val[2]));
-    assert_eq!(pt.get_momentum(), DVec3::new(arr_val[3], arr_val[4], arr_val[5]));
+    assert_eq!(pt.get_position_tangent(), DVec3::new(arr_val[0], arr_val[1], arr_val[2]));
+    assert_eq!(pt.get_momentum_tangent(), DVec3::new(arr_val[3], arr_val[4], arr_val[5]));
 }
 
 #[test]
-fn test_phase_point_arithmetic() {
+fn test_phase_tangent_arithmetic() {
     // Creating phase points
     let a1: [f64; NUM_TANGENTS] = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
     let a2: [f64; NUM_TANGENTS] = [7.0, 8.0, 9.0, 10.0, 11.0, 12.0];
-    let pt1 = PhasePoint::from_array(a1);
-    let pt2 = PhasePoint::from_array(a2);
+    let pt1 = TangentPhaseVector::from_array(a1);
+    let pt2 = TangentPhaseVector::from_array(a2);
+    let r: f64 = 1.5;
 
     // Addition tests
-    assert_eq!((pt1 + pt2).get_position(), DVec3::new(a1[0] + a2[0], a1[1] + a2[1], a1[2] + a2[2]));
-    assert_eq!((pt1 + pt2).get_momentum(), DVec3::new(a1[3] + a2[3], a1[4] + a2[4], a1[5] + a2[5]));
+    assert_eq!((pt1 + pt2).get_position_tangent(), DVec3::new(a1[0]+a2[0], a1[1]+a2[1], a1[2]+a2[2]));
+    assert_eq!((pt1 + pt2).get_momentum_tangent(), DVec3::new(a1[3]+a2[3], a1[4]+a2[4], a1[5]+a2[5]));
 
     // Subtraction tests
-    assert_eq!((pt1 - pt2).get_position(), DVec3::new(a1[0] - a2[0], a1[1] - a2[1], a1[2] - a2[2]));
-    assert_eq!((pt1 - pt2).get_momentum(), DVec3::new(a1[3] - a2[3], a1[4] - a2[4], a1[5] - a2[5]));
+    assert_eq!((pt1 - pt2).get_position_tangent(), DVec3::new(a1[0]-a2[0], a1[1]-a2[1], a1[2]-a2[2]));
+    assert_eq!((pt1 - pt2).get_momentum_tangent(), DVec3::new(a1[3]-a2[3], a1[4]-a2[4], a1[5]-a2[5]));
+
+    // Scalar multiplication tests
+    assert_eq!((pt1*r).get_position_tangent(),  DVec3::new(a1[0]*r, a1[1]*r, a1[2]*r));
+    assert_eq!((pt1*r).get_momentum_tangent(),  r*DVec3::new(a1[3], a1[4], a1[5]));
+    assert_eq!((r*pt2).get_position_tangent(),  DVec3::new(a2[0]*r, a2[1]*r, a2[2]*r));
+    assert_eq!((r*pt2).get_momentum_tangent(),  r*DVec3::new(a2[3], a2[4], a2[5]));
 }
