9747071591b4e8098e6e8c064a019e2a95659d3b
Lam Nguyen <lamtnguyen989@gmail.com>
2026-05-29 02:44:43 -0500

    making phase points

--- tests/physics_tests.rs
+++ tests/physics_tests.rs
@@ -1,9 +1,10 @@
 use billiards_logic::physics::*;
+use glam::DVec3;
 use glam::Vec3;
 
 
 #[test]
-fn collision_remains_in_box(){
+fn collision_remains_in_box() {
     let pos = Vec3::new(0.8, 0.8, 0.8);
     let vel = Vec3::new(1.0, 0.7, 0.3);
     let (new_pos, _) = collision(pos, vel).unwrap();
@@ -11,4 +12,31 @@ fn collision_remains_in_box(){
     assert!(new_pos.x >= 0.0 && new_pos.x <= BOX_SIZE);
     assert!(new_pos.y >= 0.0 && new_pos.y <= BOX_SIZE);
     assert!(new_pos.z >= 0.0 && new_pos.z <= BOX_SIZE);
+}
+
+#[test]
+fn test_phase_point_creation() {
+    assert_eq!(NUM_TANGENTS, 6);
+    let arr_val: [f64; NUM_TANGENTS] = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
+    let pt = PhasePoint::from_array(arr_val);
+
+    assert_eq!(pt.get_position(), DVec3::new(arr_val[0], arr_val[1], arr_val[2]));
+    assert_eq!(pt.get_momentum(), DVec3::new(arr_val[3], arr_val[4], arr_val[5]));
+}
+
+#[test]
+fn test_phase_point_arithmetic() {
+    // Creating phase points
+    let a1: [f64; NUM_TANGENTS] = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
+    let a2: [f64; NUM_TANGENTS] = [7.0, 8.0, 9.0, 10.0, 11.0, 12.0];
+    let pt1 = PhasePoint::from_array(a1);
+    let pt2 = PhasePoint::from_array(a2);
+
+    // Addition tests
+    assert_eq!((pt1 + pt2).get_position(), DVec3::new(a1[0] + a2[0], a1[1] + a2[1], a1[2] + a2[2]));
+    assert_eq!((pt1 + pt2).get_momentum(), DVec3::new(a1[3] + a2[3], a1[4] + a2[4], a1[5] + a2[5]));
+
+    // Subtraction tests
+    assert_eq!((pt1 - pt2).get_position(), DVec3::new(a1[0] - a2[0], a1[1] - a2[1], a1[2] - a2[2]));
+    assert_eq!((pt1 - pt2).get_momentum(), DVec3::new(a1[3] - a2[3], a1[4] - a2[4], a1[5] - a2[5]));
 }
